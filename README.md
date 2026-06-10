9cb8f709c97fd7eab6fd0e2ec002433e0d7ff3a6
Ahmad Dzuizz Annajib <ahmad.dzuizz.annajib@gmail.com>
2026-06-10 21:21:25 +0800

    v0.0.0 - v0.dzuizz.com released!

---
+++ README.md
@@ -0,0 +1,4 @@
+# v0.dzuizz.com
+
+**preface**.
+i wanted a purely minimalist exhaustive list of my accolades and things im proud of in general.
---
+++ generate.py
@@ -0,0 +1,30 @@
+import json, shutil
+from pathlib import Path
+from jinja2 import Environment, FileSystemLoader
+
+OUTPUT_DIR = Path("output")
+
+
+def load(filename):
+    return json.loads(Path(filename).read_text())
+
+
+def main() -> None:
+    if OUTPUT_DIR.exists():
+        shutil.rmtree(OUTPUT_DIR)
+
+    OUTPUT_DIR.mkdir()
+    shutil.copytree("data/certs", OUTPUT_DIR / "certs")
+
+    abbrev, achievements = load("data/abbrev.json"), load("data/achievements.json")
+
+    env = Environment(loader=FileSystemLoader("input"))
+    index_html = env.get_template("index.html.j2")
+
+    print(abbrev, achievements[0]["items"])
+    output = index_html.render(abbrev=abbrev, achievements=achievements)
+    Path("output/index.html").write_text(output)
+
+
+if __name__ == "__main__":
+    main()
